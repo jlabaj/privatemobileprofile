9098bd49975df90094cbc355168736ab029379d6
jLabaj <labaj.jakub@gmail.com>
2024-06-08 22:23:46 +0200

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jakublabaj.com.app',
+  appName: 'jakubs-profile',
+  webDir: 'dist/apps/jakubs-profile/browser'
+};
+
+export default config;
